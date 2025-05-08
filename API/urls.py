c114c14975fe11aa6332f97d3d3102eafb5b4be1
André Luiz Santos <and_bas7@hotmail.com>
2025-05-08 02:48:55 -0300

    basic-module

--- API/urls.py
+++ API/urls.py
@@ -1,14 +1,12 @@
-from home import views as home_views
-from blog import views as blog_views
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 """ MVT - Função que recebe a request, e retorna uma resposta  """
 
 """ http://127.0.0.1:800/ """
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', blog_views.blog),
-    path('', home_views.home)
+    path('blog/', include('blog.urls')),
+    path('', include('home.urls'))
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def blog(request):
     print(request)
     return HttpResponse('blog do app amo a giovanda')
+
+def exemplo(request):
+    print('exemplo')
+    return render(request, 'blog/exemplo.html')
